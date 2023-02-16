f7cf74fa1891fc47c18041c762050a0e15ce3824
Hector Rodriguez <im.hector@outlook.com>
2023-02-16 09:49:32 -0700

    Subir actividades num 5

---
+++ python5.py
@@ -0,0 +1,17 @@
+num1 = int(input("Ingrese un numero entero"))
+num2 = int(input("Ingrese otro numero entero"))
+
+operacion = num1 + num2 #Suma
+print(f"Suma: {operacion}")
+
+operacion = num2 - num1 #Resta
+print(f"Resta: {operacion}")
+
+operacion = num1 * num2 #Mullti
+print(f"Multiplicacion: {operacion}")
+
+operacion = num2 // num1 #Division entera
+print(f"Division: {operacion}")
+
+operacion = num2 % num1 #Resto division
+print(f"Resto: {operacion}")
---
+++ python5_2.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingrese un numero entero"))
+num2 = int(input("Ingrese otro numero entero"))
+
+print(f"El primer número es mayor que el segundo número: {num1>num2}")
+print(f"El primer número es menor que el segundo número: {num1<num2}")
+print(f"El primer número es igual al segundo número: {num1==num2}")
+print(f"El primer número es diferente al segundo número: {num1!=num2}")
