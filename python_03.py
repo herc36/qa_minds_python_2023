ac4cd2bd94b10d9f9b7935535685fcfe9194b4b4
Hector Rodriguez <im.hector@outlook.com>
2023-03-27 21:50:05 -0700

    ***Actividad entregable practica 3***

---
+++ python_03.py
@@ -0,0 +1,9 @@
+nombre = input("Cual es su nombre?")
+edad = input("Que edad tiene?")
+ciudad = input("En que ciudad vive?")
+casado = input("Usted esta casado?")
+num1 = int(input("Ingrese un numero entre el 1 y el 100"))
+num2 = int(input("Ingrese un numero entre 100 y 200"))
+total = num1/num2
+
+print(f"Mi nombre es {nombre}, mi edad es {edad}, soy de la ciudad de {ciudad} y mi resultado es {total}")
